86d5e9b389eaf76ecceaac1a79f9a987af557e47
HIRAKI Satoru <saruko313@gmail.com>
2012-02-03 13:25:26 +0900

    test

---
+++ test.js
@@ -0,0 +1,8 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: HIRAKI Satoru
+ * Date: 2/3/12
+ * Time: 1:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+alert('Hello, World');
